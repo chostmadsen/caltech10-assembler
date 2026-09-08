4c6edbf1bdd83ec464dbae2326262c923d402a81
chostmadsen <chostmadsen@gmail.com>
2026-09-08 11:51:12 +0200

    rehashed pseudo-ops

--- assembler/inc/common/hash_tables/pseudo.h
+++ assembler/inc/common/hash_tables/pseudo.h
@@ -26,8 +26,8 @@ static  const   hash_entry  pseudo_table[PSEUDO_TBL_S]  =   {                   
     [2]     =   { 0x6fd6d7b4540bf04a, "data",    4, { .tok=tok_data,  .grp=pseudo_segspec_t, .instr=0 } },
     [7]     =   { 0x2406a9cc2754cf2f, "code",    4, { .tok=tok_code,  .grp=pseudo_segspec_t, .instr=0 } },
     [1]     =   { 0x3be2a69d65cafeb1, "org",     3, { .tok=tok_org,   .grp=pseudo_pseudoa_t, .instr=0 } },
-    [5]     =   { 0x3d9618eb4724ec95, "include", 7, { .tok=tok_incl,  .grp=pseudo_pseudoa_t, .instr=0 } },
-    [4]     =   { 0x56d726a9de56acdc, "const",   5, { .tok=tok_const, .grp=pseudo_pseudoa_t, .instr=0 } }
+    [4]     =   { 0x56d726a9de56acdc, "const",   5, { .tok=tok_const, .grp=pseudo_pseudoa_t, .instr=0 } },
+    [5]     =   { 0x3d9618eb4724ec95, "include", 7, { .tok=tok_incl,  .grp=pseudo_pseudoa_t, .instr=0 } }
 };
 
 /*-PSEUDO-HASH-FUNCTION-----------------------------------------------------------------------------------------------*/
